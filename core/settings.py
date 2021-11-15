44e27889ee16454ced6a4c9a4f2d091271bb66b3
ilyakochankov <ilyakochankov@yandex.ru>
2021-11-15 14:08:49 +0300

    copy all files

---
+++ core/settings.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from environs import Env
+
+env = Env()
+env.read_env('.env')
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+HAARCASCADE_DIR = BASE_DIR / "services/haarcascade"
+
+DEBUG = env.bool("CV_DEBUG", default=True)
+
+CV_ALLOWED_HOSTS = env.list("CV_ALLOWED_HOSTS")
+
+CV_SECRET_KEY = env("CV_SECRET_KEY")
+
+APISPEC_SWAGGER_URL = "/api/cv/openapi.json"
+APISPEC_SWAGGER_UI_URL = "/api/cv/openapi"
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
