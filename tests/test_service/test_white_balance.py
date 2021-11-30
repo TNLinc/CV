bf1f9d2e56a814857b7f6391fd7b3054fb3c8e33
Restyled.io <commits@restyled.io>
2021-11-30 09:11:16 +0000

    Restyled by black

--- tests/test_service/test_white_balance.py
+++ tests/test_service/test_white_balance.py
@@ -6,8 +6,9 @@ from services.white_balance import LearningBasedWB
 from services.white_balance import PerfectReflectiveWB
 
 
-def test_perfect_reflection_wb_works(perfect_reflective_wb_image,
-                                     white_balance_test_image):
+def test_perfect_reflection_wb_works(
+    perfect_reflective_wb_image, white_balance_test_image
+):
     img = PerfectReflectiveWB().white_balance(white_balance_test_image)
     assert np.all(img == perfect_reflective_wb_image)
 
@@ -17,8 +18,7 @@ def test_gray_world_wb_works(gray_world_wb_image, white_balance_test_image):
     assert np.all(img == gray_world_wb_image)
 
 
-def test_learning_based_wb_works(learning_based_wb_image,
-                                 white_balance_test_image):
+def test_learning_based_wb_works(learning_based_wb_image, white_balance_test_image):
     img = LearningBasedWB().white_balance(white_balance_test_image)
     assert np.all(img == learning_based_wb_image)
 
